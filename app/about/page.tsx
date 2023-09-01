9cb3ce24bd15184914da3022f167c22303638b20
harshageorge <harshageorge4@gmail.com>
2023-09-01 16:04:27 +0100

    nested layouts created

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+    return (
+      <>
+        <h1>About</h1>
+        <p>
+          Here we'll list all the reviews.
+        </p>
+      </>
+    );
+  }
+  export default AboutPage;
---
+++ app/reviews/hollow-knight/page.tsx
@@ -0,0 +1,11 @@
+function HollowKnightPage() {
+    return (
+      <>
+        <h1>HollowKnightPage</h1>
+        <p>
+          Here we'll list all the reviews.
+        </p>
+      </>
+    );
+  }
+  export default HollowKnightPage;
