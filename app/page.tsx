7524bcc34333953c726fc5b30dec6c86466de323
harshageorge <harshageorge4@gmail.com>
2023-11-02 17:51:36 +0000

    Featured review completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
 import Heading from "@/components/Heading";
+import { getFeaturedReview } from "@/lib/reviews";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const featuredReview = await getFeaturedReview();
     return (
       <>
-        <Heading>Indie Gamer</Heading>
+      <Heading>Indie Gamer</Heading>
         <p className="pb-3">
           Only the best indie games, reviewed for you.
         </p>
         <div className="bg-white border rounded w-80 sm:w-full shadow hover:shadow-xl">
-          <Link href="/reviews/stardew-valley" className="flex flex-col sm:flex-row">
+          <Link href={`/reviews/${featuredReview.slug}`} className="flex flex-col sm:flex-row">
             <img
-              src="/images/stardew-valley.jpg"
+              src={featuredReview.image}
               alt=""
               width="320"
               height="180"
               className="rounded-t sm:rounded-l sm:rounded-r-none"
             />
-            <h2 className="py-1 font-orbitron font-semibold text-center sm:px-2">Stardew-valley</h2>
+            <h2 className="py-1 font-orbitron font-semibold text-center sm:px-2">{featuredReview.title}</h2>
           </Link>
         </div>
       </>
